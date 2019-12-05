65c6a13ff07b207ae9b72378b5bc570b6af55395
Briquelebrit <miguelmejiajimenez@hotmail.com>
2019-12-05 16:19:11 +0100

    compile.bat updated

--- compile.bat
+++ compile.bat
@@ -1,8 +1,2 @@
-REM pattern library
-REM bison --defines=pattern/patternparser.hpp pattern/pattern.y -o pattern/patternparser.cpp &&
-REM flex -o pattern/patternlexer.cpp pattern/pattern.l &&
-REM expression library
-REM bison --defines=expression/expressionparser.hpp expression/expression.y -o expression/expressionparser.cpp &&
-REM flex -o expression/expressionlexer.cpp expression/expression.l &&
 
-g++ -mwin32 -Ilib -Ithirdparty -std=c++11 src\\*.cpp -o TASCHE
+g++ -mwin32 -DYY_NO_UNISTD_H -Ilib -Ithirdparty -std=c++11 src\expression.cpp src\expressionlexer.cpp src\expressionparser.cpp src\pattern.cpp src\patternlexer.cpp src\patternparser.cpp src\responses.cpp src\main.cpp -o TASCHE
