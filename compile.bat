04dac73f17e0fb8c01f01eafe28c6d1dafd062f8
Briquelebrit <miguelmejiajimenez@hotmail.com>
2020-08-15 01:43:39 +0200

    deleted old compile scripts

--- compile.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-g++ -mwin32 -std=c++11 -DYY_NO_UNISTD_H -Iinclude -Ithirdparty src\expression.cpp src\expressionlexer.cpp src\expressionparser.cpp src\pattern.cpp src\patternlexer.cpp src\patternparser.cpp src\responses.cpp src\symboltable.cpp src\util.cpp src\main.cpp -o TASCHE
